99875c3a6ae751c51909097c77d8473212a71cd8
ashwanikd0123 <ashwanikd0123@gmail.com>
2025-09-23 02:39:34 +0530

    comment removed clean code

--- app/src/main/java/com/example/thebarometer/weathermodel/retrofitfetch/OpenWeatherApi.kt
+++ app/src/main/java/com/example/thebarometer/weathermodel/retrofitfetch/OpenWeatherApi.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 interface OpenWeatherApi {
     @GET("forecast")
     suspend fun getThreeDayForecast(
-        @Query("q") city: String,            // e.g. "Lucknow,IN"
+        @Query("q") city: String,
         @Query("units") units: String = "metric",
         @Query("appid") apiKey: String
     ): ForecastResponse
